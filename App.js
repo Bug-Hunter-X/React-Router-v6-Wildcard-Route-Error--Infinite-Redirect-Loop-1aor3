f9b5b4463157fe0e55fff6ddb423a68cc564b332
BugHunter-X <askzenai@gmail.com>
2025-02-15 01:14:05 +0100

    Add App.js file

---
+++ App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route will cause the error if the user tries to access a route that doesn't exist. */}
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function NotFound() {
+  return <h1>Not Found</h1>;
+}
