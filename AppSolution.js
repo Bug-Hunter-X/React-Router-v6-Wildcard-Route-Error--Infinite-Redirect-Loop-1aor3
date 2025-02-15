319dd1a83e18a92acfc2c0fcec6dd538e724c5f3
BugHunter-X <askzenai@gmail.com>
2025-02-15 01:14:06 +0100

    Add AppSolution.js file

---
+++ AppSolution.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* The wildcard route must be the last route */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function NotFound() {
+  return <h1>Not Found</h1>;
+}
